3fe4354eded9befad9f707bdd18aac114b2e6a77
WillS78 <101777677+WillS78@users.noreply.github.com>
2022-05-07 14:26:13 -0600

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -27,30 +27,26 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
 CREATE TABLE dept_emp (
-	dept_no varchar NOT NULL,
 	emp_no int NOT NULL,
+	dept_no varchar NOT NULL,
 	from_date date NOT NULL,
 	to_date date NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
-
 CREATE TABLE titles (
 	emp_no int NOT NULL,
 	title varchar NOT NULL,
 	from_date date NOT NULL,
 	to_date date NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 SELECT * FROM departments;
